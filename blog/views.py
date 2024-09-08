e1fb71d2dab51d40e1a0f28633b5b9b79fab1f9e
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2024-09-08 19:14:19 +0200

    v3-v4

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+posts=[
+    {
+        "author":"youssef",
+        "title":"test1",
+        "content":" skdsjkhfsfhsdf ",
+        "date_posted":"August 27 ,2018",
+    },
+    {
+        "author":"nader",
+        "title":" welcome",
+        "content":"second post content ",
+        "date_posted":"August 27 ,2018",
+    }
+]
+
 def home(request):
-    return HttpResponse("welcome")
+    context={"posts":posts}
+    return render(request,"blog/home.html",context)
 def about(request):
-    return HttpResponse("about")
+    return render(request,"blog/about.html",{"title":"about"})
+    # return HttpResponse("about")

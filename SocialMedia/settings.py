9de96b7c7a8e6f8f91d3c7f6dad1095c815057d5
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2024-09-23 02:22:01 +0300

    v6

--- SocialMedia/settings.py
+++ SocialMedia/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog', #blog.apps.BlogConfig'
+    'users',
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"

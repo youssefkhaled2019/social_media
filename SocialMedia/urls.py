6721716edd258eeaa4210d0da21095b837fd7ded
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2024-09-27 23:41:48 +0300

    v_8

--- SocialMedia/urls.py
+++ SocialMedia/urls.py
@@ -19,6 +19,8 @@ from django.urls import path,include
 from users import views as users_views
 from django.contrib.auth import views as auth_views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name="users/login.html") ,name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html",next_page='login') ,name="logout"),
 ]
+
+if settings.DEBUG:
+   urlpatterns+=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/models.py
+++ users/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Profile(models.Model):
+ user_id=models.OneToOneField(User,on_delete=models.CASCADE)
+ image=models.ImageField(default="default.jpg",upload_to="profile_pics")
+ def __str__(self):
+  return f'{ self.user_id.username} profile '
+ 
 
 
 # unique email
-from django.contrib.auth.models import User
-User._meta.get_field('email')._unique = True
 
-# Create your models here.
+User._meta.get_field('email')._unique = True

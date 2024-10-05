8046c8a51b0e0a1e6a75e35b8160cad2d8bc9576
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2024-10-05 21:36:59 +0300

    v_10

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 
 # Create your models here.
@@ -17,7 +17,8 @@ class Post(models.Model):
         return self.title
     
     
-    
+    def get_absolute_url(self):
+        return reverse('post-detail',kwargs={"pk":self.pk})
     
     
     
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 from .models import Post
 from django.contrib.auth.decorators import login_required
 
+from django.forms.models import BaseModelForm
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.views.generic import (ListView,
+                                  DetailView,
+                                  CreateView,UpdateView,DeleteView)
 # Create your views here.
 
 
@@ -14,3 +19,48 @@ def home(request):
 def about(request):
     return render(request,"blog/about.html",{"title":"about"})
     # return HttpResponse("about")
+
+
+class PostListView(LoginRequiredMixin,ListView):
+    model=Post
+    template_name ="blog/home.html" #<app>/<model>_<viewtype>.html
+    context_object_name="posts"
+    ordering=["-date_posted"]
+
+
+
+class PostDetailView(DetailView): #    template_name_suffix = "_detail"
+    model=Post
+
+
+class PostCreateView(LoginRequiredMixin,CreateView):#    template_name_suffix = "_form" ex:post_form 
+    model=Post  
+    fields=['title','content']
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):#template_name_suffix = "_form" ex:post_form 
+    model=Post  
+    fields=['title','content']
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        form.instance.author=self.request.user
+        return super().form_valid(form)    
+    
+    def test_func(self) -> bool | None: #UserPassesTestMixin
+        post =self.get_object()
+        if (self.request.user==post.author):
+            return True
+        return False
+        
+
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView):#   template_name_suffix = "_confirm_delete" ex:post_confirm_delete.html
+    model=Post
+    success_url="/"
+    def test_func(self) -> bool | None:
+        post =self.get_object()
+        if (self.request.user==post.author):
+            return True
+        return False

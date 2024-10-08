aeec85573c936598e7da4d93f8f649a2828d44d5
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2024-10-09 01:11:18 +0300

    v_12

--- SocialMedia/settings.py
+++ SocialMedia/settings.py
@@ -134,3 +134,12 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL="blog-home" # redirect after log in 
 LOGIN_URL="login" # @login_required
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER ='youssef.khaled1990@gmail.com' #os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD ='tvsb ggli saxs kids' #os.environ.get('EMAIL_PASS')
